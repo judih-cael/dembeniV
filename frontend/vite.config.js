710afa49725a5e5d11782e7bcf3b1a7749cfabce
judih-cael <judih@cael.com>
2026-06-25 09:34:55 +0300

    trdfgjbvfs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dembeni/',
-  server: {
-    port: 5173,
-    open: false,
-    strictPort: false
-  },
-  test: {
-    environment: 'jsdom'
-  }
+  base: '/dembeni/'
 })
